c114e666225bc94e413490edb541bef4af526106
Elhaw2003 <ebrahimelhaw0@gmail.com>
2024-08-18 22:32:19 +0300

    this project already finshed by perfect quistions

--- lib/featuers/home/presentation/controller/home_provider.dart
+++ lib/featuers/home/presentation/controller/home_provider.dart
@@ -8,15 +8,16 @@ import 'package:quiz_app/featuers/home/presentation/view/widgets/show_dialog.dar
 
 class HomeProvider extends ChangeNotifier{
   List <QuestionModel> qustionList = [
-
     QuestionModel(title: "كم عدد سور القرآن الكريم؟", answers: ["110","100","90","114"], correctAnswer: "114", selectedAnswer: null),
-    QuestionModel(title: "أول من آمن من الرجال ؟", answers: ["ابوبكر","عمر بن الحطاب","علي بن ابي طالب","عثمان بن عفان"], correctAnswer: "ابوبكر", selectedAnswer: null),
+    QuestionModel(title: "من هو أول من آمن برسالة النبي محمد صلى الله عليه وسلم من الرجال؟", answers: ["ابوبكر","عمر بن الحطاب","علي بن ابي طالب","عثمان بن عفان"], correctAnswer: "ابوبكر", selectedAnswer: null),
     QuestionModel(title: "كم عدد أجزاء القرآ الكريم؟", answers: ["110","40","30","35"], correctAnswer: "30", selectedAnswer: null),
-    QuestionModel(title: "كم عدد سور القرآن الكريم؟", answers: ["110","100","90","114"], correctAnswer: "114", selectedAnswer: null),
-    QuestionModel(title: "كم عدد سور القرآن الكريم؟", answers: ["110","100","90","114"], correctAnswer: "114", selectedAnswer: null),
+    QuestionModel(title: "ما هو أول مسجد بني في الإسلام؟", answers: ["المسجد الحرام","المسجد النبوي","مسجد قباء","المسجد الأقصى"], correctAnswer: "مسجد قباء", selectedAnswer: null),
+    QuestionModel(title: "كم عدد أركان الإسلام؟", answers: [" ثلاثة"," أربعة","خمسة"," ستة"], correctAnswer: "خمسة", selectedAnswer: null),
+    QuestionModel(title: "ما هي السورة التي تعادل ثلث القرآن؟", answers: ["سورة الإخلاص","سورة الفاتحة","سورة الكوثر"," سورة الناس"], correctAnswer: "سورة الإخلاص", selectedAnswer: null),
+    QuestionModel(title: "ما هو اسم والد النبي محمد صلى الله عليه وسلم؟", answers: ["عبد المطلب","عبد الله","أبو طالب","أبو لهب"], correctAnswer: "عبد الله", selectedAnswer: null),
+    QuestionModel(title: "في أي شهر فرض صيام رمضان؟", answers: ["محرم","رجب","شعبان","رمضان"], correctAnswer: "شعبان", selectedAnswer: null),
+    QuestionModel(title: "ما هو الإسم الأول للنبي محمد صلى الله عليه وسلم قبل البعثة؟", answers: ["الصادق","الأمين","الشريف","الحكيم"], correctAnswer: "الأمين", selectedAnswer: null)
   ];
-
-
   int quistionIndex = 0;
   int count = 1;
   int score = 0;
